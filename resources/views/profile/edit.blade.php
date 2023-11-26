96645ca7f21c6e5d98b2b0e0b364fa8d96248b16
bagusherma <bagusherma123@gmail.com>
2023-11-26 12:55:48 +0700

    fix profile, change pw, delete acc

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -105,51 +105,149 @@
                             </p>
                         </div>
                         <div class="card-body">
-                            <form action="#" method="get">
+                            <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+                                @csrf
+                                @method('put')
                                 <div class="form-group my-2">
                                     <label for="current_password" class="form-label">Current Password</label>
                                     <input type="password" name="current_password" id="current_password"
-                                        class="form-control" placeholder="Enter your current password"
-                                        value="1L0V3Indonesia">
-                                </div>
-                                <div class="form-group my-2">
-                                    <label for="password" class="form-label">New Password</label>
-                                    <input type="password" name="password" id="password" class="form-control"
-                                        placeholder="Enter new password" value="">
-                                </div>
-                                <div class="form-group my-2">
-                                    <label for="confirm_password" class="form-label">Confirm Password</label>
-                                    <input type="password" name="confirm_password" id="confirm_password"
-                                        class="form-control" placeholder="Enter confirm password" value="">
-                                </div>
+                                        class="form-control" placeholder="Enter your current password" value="">
+                                    <div style="color:rgb(248 113 113);">
+                                        @if ($errors->updatePassword->has('current_password'))
+                                            {{ $errors->updatePassword->first('current_password') }}
+                                        @endif
+                                    </div>
+                                    <div class="form-group my-2">
+                                        <label for="password" class="form-label">New Password</label>
+                                        <input type="password" name="password" id="password" class="form-control"
+                                            placeholder="Enter new password" value="">
+                                        <div style="color:rgb(248 113 113);">
+                                            @if ($errors->updatePassword->has('password'))
+                                                {{ $errors->updatePassword->first('password') }}
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="form-group my-2">
+                                        <label for="password_confirmation" class="form-label">Confirm Password</label>
+                                        <input type="password" name="password_confirmation" id="password_confirmation"
+                                            class="form-control" placeholder="Enter confirm password" value="">
+                                        <div style="color:rgb(248 113 113);">
+                                            @if ($errors->updatePassword->has('password_confirmation'))
+                                                {{ $errors->updatePassword->first('password_confirmation') }}
+                                            @endif
+                                        </div>
+                                    </div>
 
-                                <div class="form-group my-2 d-flex justify-content-end">
-                                    <button type="submit" class="btn btn-primary">Save Changes</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-
-                    <div class="card">
-                        <div class="card-header">
-                            <h5 class="card-title">Delete Account</h5>
-                        </div>
-                        <div class="card-body">
-                            <form action="#" method="get">
-                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                                    Once your account is deleted, all of its resources and data will be permanently deleted.
-                                    Before deleting your account, please download any data or information that you wish to
-                                    retain.
-                                </p>
-                                <div class="form-group my-2 d-flex justify-content-end">
-                                    <button type="submit" class="btn btn-danger" id="btn-delete-account"
-                                        disabled="">Delete</button>
-                                </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary">Save Changes</button>
+                                    </div>
+                                    @if (session('status') === 'password-updated')
+                                        <div class="alert alert-success alert-dismissible show fade">
+                                            Saved
+                                            <button type="button" class="btn- sm btn-close" data-bs-dismiss="alert"
+                                                aria-label="Close"></button>
+                                        </div>
+                                    @endif
                             </form>
                         </div>
                     </div>
                 </div>
+
+
+
+
+                <div class="card">
+                    <div class="card-header">
+
+                        <h5 class="card-title">
+                            {{ __('Delete Account') }}
+                        </h5>
+
+                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+                        </p>
+
+                        <div class="form-group">
+                            <button class="btn btn-danger"
+                                onclick="openConfirmUserDeletionModal()">DELETE ACCOUNT</button>
+                        </div>
+                        <div id="confirm-user-deletion" class="modal fade" tabindex="-1">
+                            <div class="modal-dialog">
+                                <form method="post" action="{{ route('profile.destroy') }}" class="modal-content p-4">
+                                    @csrf
+                                    @method('delete')
+
+                                    <h2 class="card-title">
+                                        {{ __('Are you sure you want to delete your account?') }}
+                                    </h2>
+
+                                    <p class="mt-1 text-sm text-muted">
+                                        {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                                    </p>
+
+                                    <div class="mt-3">
+                                        <label for="password"
+                                            class="form-label visually-hidden">{{ __('Password') }}</label>
+                                        <input id="password" name="password" type="password" class="form-control"
+                                            placeholder="{{ __('Password') }}" />
+
+                                        <div style="color:rgb(248 113 113);">
+                                            @if ($errors->userDeletion->has('password'))
+                                                <script>
+                                                    // Buka modal secara otomatis jika ada kesalahan password
+                                                    document.addEventListener("DOMContentLoaded", function() {
+                                                        openConfirmUserDeletionModal();
+                                                    });
+                                                </script>
+                                                {{ $errors->userDeletion->first('password') }}
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="mt-4 d-flex justify-content-end">
+                                        <button type="button" class="btn btn-secondary"
+                                            onclick="closeConfirmUserDeletionModal()">{{ __('Cancel') }}</button>
+                                        <button type="submit"
+                                            class="btn btn-danger ms-3">{{ __('Delete Account') }}</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <script>
+                    function openConfirmUserDeletionModal() {
+                        var modal = new bootstrap.Modal(document.getElementById('confirm-user-deletion'));
+                        modal.show();
+                    }
+
+                    function closeConfirmUserDeletionModal() {
+                        var modal = new bootstrap.Modal(document.getElementById('confirm-user-deletion'));
+                        modal.hide();
+                    }
+                </script>
+
             </div>
         </section>
     </div>
 @endsection
+@push('page-script')
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var confirmUserDeletionModal = new bootstrap.Modal(document.getElementById('confirm-user-deletion'));
+
+            function openConfirmUserDeletionModal() {
+                confirmUserDeletionModal.show();
+            }
+
+            function closeConfirmUserDeletionModal() {
+                confirmUserDeletionModal.hide();
+            }
+
+            window.openConfirmUserDeletionModal = openConfirmUserDeletionModal;
+            window.closeConfirmUserDeletionModal = closeConfirmUserDeletionModal;
+        });
+    </script>
+@endpush
