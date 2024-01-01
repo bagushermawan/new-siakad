a520053171f58e010e841df91338f1a9e28fe0f4
bagusherma <bagusherma123@gmail.com>
2024-01-02 02:28:36 +0700

    delete comment @forbiddenexception

--- app/Exceptions/ForbiddenException.php
+++ app/Exceptions/ForbiddenException.php
@@ -7,14 +7,7 @@ use Throwable;
 
 class ForbiddenException extends Exception
 {
-    /**
-     * Create a new ForbiddenException instance.
-     *
-     * @param string $message
-     * @param int $code
-     * @param \Throwable|null $previous
-     * @return void
-     */
+
     public function __construct($message = 'Maaf, akun anda tidak memiliki hak akses untuk halaman ini.', $code = 403, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
