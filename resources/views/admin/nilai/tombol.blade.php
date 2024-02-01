1108512d6edf0229e15ddb64bae16b782b07de9a
bagusherma <bagusherma123@gmail.com>
2024-02-02 01:32:01 +0700

    edit tombol nilai

--- resources/views/admin/nilai/tombol.blade.php
+++ resources/views/admin/nilai/tombol.blade.php
@@ -1,4 +1,14 @@
-<center>
-    <a href='#' data-id="{{ $data->id }}" class="badge bg-primary tombol-edit">Edit</a> |
-<a href='#' data-id="{{ $data->id }}" data-name="{{ $data->user->name }}" class="badge bg-danger tombol-del">Del</a>
-</center>
+<div class="btn-group mb-1">
+    <div class="dropdown">
+        <div class="btn-group">
+            <button class="btn btn-primary btn-sm dropdown-toggle me-1" type="button" id="dropdownMenuButtonIcon"
+                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="bi bi-error-circle me-50"></i> Action
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButtonIcon" style="">
+                <a class="dropdown-item tombol-edit" href="#" data-id="{{ $data->id }}">Edit</a>
+                <a class="dropdown-item tombol-del" href="#" data-id="{{ $data->id }}" data-name="{{ $data->name }}">Delete</a>
+            </div>
+        </div>
+    </div>
+</div>
